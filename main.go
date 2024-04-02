4af63895d7ed0d9da6d3e1902e3b4205dab2a46f
Robert Taylor <robert.taylor2@arm.com>
2024-04-02 09:29:23 +0100

    Added CLoptions for token

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"flag"
-	
+
 	"murvoth/legend-bot/bot"
 )
 
-
+var (
+	GuildID = flag.String("guild", "", "Test Guild ID. Leaving empty will register commands globally.")
+	BotToken = flag.String("token", "", "Bot access token")
+)
 
 func main() {
-	bot.Run()
+	flag.Parse()
+	bot.Run(*GuildID, *BotToken)
 }
