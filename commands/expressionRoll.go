443a9db154f0a3187c6826fef2f41e850ea3af1e
Robert Taylor <robert.taylor2@arm.com>
2024-04-02 14:37:04 +0100

    Moved roll logic into own function

--- commands/expressionRoll.go
+++ commands/expressionRoll.go
@@ -2,9 +2,8 @@ package commands
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
 	"strings"
+	"murvoth/legend-bot/utility"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -33,35 +32,17 @@ var ExpressionRoll = &discordgo.ApplicationCommand{
 func ExpressionRollHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.ApplicationCommandData().Name == "r" {
 		options := i.ApplicationCommandData().Options
-		expression := strings.ReplaceAll(options[0].StringValue(), " ", "")
 
-		total := 0
-		rolls := make([]int, 0)
+		expression := strings.ReplaceAll(options[0].StringValue(), " ", "")
 
 		user := i.Member
 
-		// split expression into component parts
-		initialSplit := strings.Split(expression, "+")
-		for _, v := range initialSplit {
-			if strings.Contains(v, "d") {
-				// split roll find type and number of dice
-				roll := strings.Split(v, "d")
-				num := roll[0]
-				sides, err := strconv.Atoi(roll[1])
-				if err != nil {fmt.Printf("Error converting sides to int: %v", err)}
-				// roll dice and add value to total
-				for range num {
-					roll := rand.Intn(sides) + 1
-					rolls = append(rolls, roll)
-					total += roll
-				}
-			} else {
-				// add bonuses to roll total
-				bonus, err := strconv.Atoi(v)
-				if err != nil {fmt.Println("Error converting bonus to int", err)}
-				total += bonus
-			}
+		total, rolls, err := utility.ExpressionRoll(expression)
+		if err != nil {
+			fmt.Println(err)
 		}
+
+		rollsString := utility.ConvertRollsToString(rolls)
 
 		embed := &discordgo.MessageEmbed {
 			Title: fmt.Sprintf("%s\nTotal: %v", expression, total),
@@ -69,7 +50,7 @@ func ExpressionRollHandler(s *discordgo.Session, i *discordgo.InteractionCreate)
 				Name: fmt.Sprintf("%s for %v", user.Nick, options[1].StringValue()),
 				IconURL: user.User.AvatarURL(""),
 			},
-			
+			Description: rollsString,
 		}
 
 		if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
---
+++ utility/roll.go
@@ -0,0 +1,39 @@
+package utility
+
+import (
+	"strings"
+	"strconv"
+	"math/rand"
+	"fmt"
+)
+
+func ExpressionRoll(exp string) (int, []int, error) {
+
+		total := 0
+		rolls := make([]int, 0)
+
+	initialSplit := strings.Split(exp, "+")
+		for _, v := range initialSplit {
+			if strings.Contains(v, "d") {
+				// split roll find type and number of dice
+				roll := strings.Split(v, "d")
+				num := roll[0]
+				sides, err := strconv.Atoi(roll[1])
+				if err != nil {
+					return 0, nil, fmt.Errorf("error converting sides to int: %v", err)
+				}
+				// roll dice and add value to total
+				for range num {
+					roll := rand.Intn(sides) + 1
+					rolls = append(rolls, roll)
+					total += roll
+				}
+			} else {
+				// add bonuses to roll total
+				bonus, err := strconv.Atoi(v)
+				if err != nil {fmt.Println("Error converting bonus to int", err)}
+				total += bonus
+			}
+		}
+	return total, rolls, nil
+}
