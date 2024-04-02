9d0aec297c11bed4c1c998f6f4618b9cac83db4c
Robert Taylor <robert.taylor2@arm.com>
2024-04-02 10:15:36 +0100

    Added error handling

--- commands/ping.go
+++ commands/ping.go
@@ -6,19 +6,18 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var PingCommand = &discordgo.ApplicationCommand {
-	Name: "ping",
+var PingCommand = &discordgo.ApplicationCommand{
+	Name:        "ping",
 	Description: "Replies with Pong!",
 }
 
 func PingHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	fmt.Println("running ping command")
 	if i.ApplicationCommandData().Name == "ping" {
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{Content: "Pong!"},
-		})
-		if err != nil {
+		}); err != nil {
 			fmt.Println(err)
 		}
 	}
