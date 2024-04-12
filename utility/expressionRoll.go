06959f0b109d79a610cb325dac650d313955f681
Robert Taylor <robert.taylor2@arm.com>
2024-04-12 14:17:44 +0100

    Allow "d20" as valid expression

--- utility/expressionRoll.go
+++ utility/expressionRoll.go
@@ -1,10 +1,10 @@
 package utility
 
 import (
-	"strings"
-	"strconv"
-	"math/rand"
 	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
 )
 
 type Roll struct {
@@ -14,34 +14,42 @@ type Roll struct {
 
 func ExpressionRoll(exp string) (int, []Roll, error) {
 
-		total := 0
-		rolls := make([]Roll, 0)
+	total := 0
+	rolls := make([]Roll, 0)
 
 	initialSplit := strings.Split(exp, "+")
-		for _, v := range initialSplit {
-			if strings.Contains(v, "d") {
-				// split roll find type and number of dice
-				roll := strings.Split(v, "d")
-				num, err := strconv.Atoi(roll[0])
+	for _, v := range initialSplit {
+		if strings.Contains(v, "d") {
+			// split roll find type and number of dice
+			roll := strings.Split(v, "d")
+			num := 0
+			if roll[0] == "" {
+				num = 1
+			} else {
+				numb, err := strconv.Atoi(roll[0])
 				if err != nil {
 					return 0, nil, fmt.Errorf("error converting number of dice to int: %v", err)
 				}
-				sides, err := strconv.Atoi(roll[1])
-				if err != nil {
-					return 0, nil, fmt.Errorf("error converting sides to int: %v", err)
-				}
-				// roll dice and add value to total
-				for range num {
-					roll := rand.Intn(sides) + 1
-					rolls = append(rolls, Roll{sides, roll})
-					total += roll
-				}
-			} else {
-				// add bonuses to roll total
-				bonus, err := strconv.Atoi(v)
-				if err != nil {fmt.Println("Error converting bonus to int", err)}
-				total += bonus
+				num = numb
 			}
+			sides, err := strconv.Atoi(roll[1])
+			if err != nil {
+				return 0, nil, fmt.Errorf("error converting sides to int: %v", err)
+			}
+			// roll dice and add value to total
+			for range num {
+				roll := rand.Intn(sides) + 1
+				rolls = append(rolls, Roll{sides, roll})
+				total += roll
+			}
+		} else {
+			// add bonuses to roll total
+			bonus, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Println("Error converting bonus to int", err)
+			}
+			total += bonus
 		}
+	}
 	return total, rolls, nil
 }
