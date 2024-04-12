7cec745eeb3d6fc268d28a3a9d8eeb3d5ae7723e
Robert Taylor <robert.taylor2@arm.com>
2024-04-12 14:08:16 +0100

    Pull token and appId form env

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"murvoth/legend-bot/bot"
 )
 
 var (
 	GuildID = flag.String("guild", "", "Test Guild ID. Leaving empty will register commands globally.")
-	BotToken = flag.String("token", "", "Bot access token")
-	AppID = flag.String("appid", "", "App ID")
+	// BotToken = flag.String("token", "", "Bot access token")
+	// AppID = flag.String("appid", "", "App ID")
 )
 
 func main() {
+	appID := os.Getenv("APP_ID")
+	botToken := os.Getenv("BOT_TOKEN")
 	flag.Parse()
-	bot.Run(*AppID, *GuildID, *BotToken)
+	fmt.Println(appID)
+	fmt.Println(botToken)
+	bot.Run(appID, *GuildID, botToken)
 }
