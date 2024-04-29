9f95d47363e1ca48ab59d9bd08209c2706238f24
Robert Taylor <robert.taylor2@arm.com>
2024-04-29 14:40:40 +0100

    Added utilities for custom rolls

--- utility/embedMaker.go
+++ utility/embedMaker.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func EmbedMaker(exp, rollFor string, total int, rolls []Roll, user *discordgo.Member) *discordgo.MessageEmbed {
+func RollEmbedMaker(exp, rollFor string, total int, rolls []Roll, user *discordgo.Member) *discordgo.MessageEmbed {
 
 	name := user.Nick
 	if rollFor != "" {
@@ -22,6 +22,32 @@ func EmbedMaker(exp, rollFor string, total int, rolls []Roll, user *discordgo.Me
 		Image: &discordgo.MessageEmbedImage{
 			URL: "attachment://image.png",
 		},
+	}
+	return embed
+}
+
+func CustomDiceEmbedMaker(rolls UserRolls, user *discordgo.Member) *discordgo.MessageEmbed{
+	embed := &discordgo.MessageEmbed{
+		Title: fmt.Sprintf("%s Custom Rolls", user.Nick),
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    user.Nick,
+			IconURL: user.AvatarURL(""),
+		},
+	}
+	for _, r := range rolls.Rolls {
+		if r.Type == "attack" {
+			emField := &discordgo.MessageEmbedField{
+				Name:  r.Name,
+				Value: fmt.Sprintf("Attack Bonus: %s\nDamage: %s + %s\nType: %s", r.Expression, r.DamageExp, r.DamageBonus, r.Type),
+			}
+			embed.Fields = append(embed.Fields, emField)
+		} else {
+			emField := &discordgo.MessageEmbedField{
+				Name:  r.Name,
+				Value: fmt.Sprintf("Roll: %s", r.Expression),
+			}
+			embed.Fields = append(embed.Fields, emField)
+		}
 	}
 	return embed
 }
